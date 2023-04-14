cc1d9e83ea30dad44f9ac8b4e03358ff086c1c77
Sandaru Muthuwadige <sandarumuthuwadige@gmail.com>
2023-04-15 01:54:29 +0530

    Validation Functio

--- client/src/Check.js
+++ client/src/Check.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 function Check() {
 
-    const submitApplication = (e) => {
+    const checkStatus = (e) => {
         e.preventDefault();
         var form = document.querySelector("#formElement");
         console.log(form)
@@ -18,6 +18,7 @@ function Check() {
             window.location.reload();
         });
     };
+
 
     return (
         <>
@@ -42,7 +43,7 @@ function Check() {
                 <div class="column is-half is-offset-one-quarter">
 
                     <section class="section">
-                        <h1 class="title">Document Submission Form</h1>
+                        <h1 class="title">Check Verification Status</h1>
                         <form id="formElement" encType="multipart/form-data" method="post">
                             <div class="field">
                                 <label class="label">Wallet Address</label>
@@ -53,90 +54,9 @@ function Check() {
 
                             <div class="field">
                                 <button id="walletbtn" class="button is-primary is-rounded"
-                                    onClick={async (e) => {
-                                        e.preventDefault();
-                                        document.getElementById('walletbtn').className = "button is-primary is-rounded is-loading";
-                                        const account = await window.ethereum.request({ method: "eth_requestAccounts" })
-                                        console.log("Account retrieved :", account)
-                                        var delayInMilliseconds = 500;
-                                        setTimeout(function () {
-                                            document.getElementById('wallet').value = account;
-                                            document.getElementById('walletbtn').className = "button is-primary is-rounded";
-                                            document.getElementById('walletbtn').innerHTML = "Connected";
-                                        }, delayInMilliseconds);
-                                    }}>Connect Wallet</button>
+                                    onClick={checkStatus}>Check</button>
                             </div>
 
-                            <div class="field">
-                                <label class="label">Full Name</label>
-                                <div class="control">
-                                    <input type="text" name='fname' class="input is-rounded" />
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <label class="label">NIC Number</label>
-                                <div class="control">
-                                    <input type="text" name='nic' class="input is-rounded" />
-                                </div>
-                            </div>
-                            <br />
-
-                            <div class="field">
-                                <label class="label">NIC Front</label><br />
-                                <div class="file is-centered is-boxed is-success has-name">
-                                    <label class="file-label">
-                                        <input name="uploaded_file" class="file-input" type="file" />
-                                        <span class="file-cta">
-                                            <span class="file-label">Choose (JPEG/PNG)</span>
-                                        </span>
-                                        <span class="file-name is-center">Image.png</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <br />
-
-                            <div class="field">
-                                <label class="label">NIC Back</label><br />
-                                <div class="file is-centered is-boxed is-success has-name">
-                                    <label class="file-label">
-                                        <input class="file-input" type="file" name="uploaded_file" />
-                                        <span class="file-cta">
-                                            <span class="file-label">Choose (JPEG/PNG)</span>
-                                        </span>
-                                        <span class="file-name is-center">Image.png</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <br />
-
-                            <div class="field">
-                                <label class="label">Upload a selfie with the NIC</label><br />
-                                <div class="file is-centered is-boxed is-success has-name">
-                                    <label class="file-label">
-                                        <input class="file-input" type="file" name="uploaded_file" />
-                                        <span class="file-cta">
-                                            <span class="file-label">Choose (JPEG/PNG)</span>
-                                        </span>
-                                        <span class="file-name is-center">Image.png</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <br />
-
-                            <div class="field">
-                                <div class="control">
-                                    <label class="checkbox">
-                                        <input type="checkbox" /> I agree to the <a href="/">terms and conditions</a>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div class="field is-grouped">
-                                <div class="control">
-                                    <button onClick={submitApplication} class="button is-primary is-rounded">Submit for Review</button>
-                                </div>
-                            </div>
                         </form>
                     </section>
                 </div>
